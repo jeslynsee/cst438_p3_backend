72a910c64b0ec2f2782e17d87be1f2c534b4b77e
SpIurge <sergonzalez@csumb.edu>
2025-10-23 13:12:29 -0700

    initializing spring boot

---
+++ src/main/java/com/example/cvd/CvdApplication.java
@@ -0,0 +1,13 @@
+package com.example.cvd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cvd/CvdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cvd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
