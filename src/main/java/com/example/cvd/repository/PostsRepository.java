41a7303f8d17bd91c5f8c3aa9aca5c97b6cd31e2
jeslynsee <jeslynesee@gmail.com>
2025-11-17 21:08:10 -0800

    Add route to get all posts by user's team. Add custom function definition in repo file

--- src/main/java/com/example/cvd/repository/PostsRepository.java
+++ src/main/java/com/example/cvd/repository/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.example.cvd.repository;
 
 import com.example.cvd.entity.Posts;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,4 +16,7 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
     @Transactional
     @Query("UPDATE Posts p SET p.likes = p.likes + 1 WHERE p.id = :id")
     int incrementLikes(@Param("id") Long id);
+
+    // have to name function getPostsByUser_Team to show we are joining user and posts tables, since we are grabbing team from user
+    List<Posts> getPostsByUser_Team(String team);
 }
